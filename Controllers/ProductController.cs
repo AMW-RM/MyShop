ca84ee70e2bd5cbfb71efe9925c5fca445712788
Rafał Mąka <r.maka@amw-it.pl>
2025-03-27 18:05:49 +0100

    uruchomienie strony v1

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyShop.Models;
+
+namespace MyShop.Controllers;
+
+public class ProductController : Controller
+{
+    private readonly IProductRepository _productRepository;
+    private readonly ICategoryRepository _categoryRepository;
+
+    public ProductController(IProductRepository productRepository, ICategoryRepository categoryRepository)
+    {
+        _productRepository = productRepository;
+        _categoryRepository = categoryRepository;
+    }
+
+    public  IActionResult List()
+    {
+        ViewBag.CurrentCategory = "chemia";
+        var products = _productRepository.AllProducts;
+        return View(products);
+    }
+}
