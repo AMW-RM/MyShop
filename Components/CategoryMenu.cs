fd09cefaf233059d62bb484fd729a111cae21584
Rafał Mąka <r.maka@amw-it.pl>
2025-05-22 18:01:02 +0200

    CategoryMenu komponent widoku cz1

---
+++ Components/CategoryMenu.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyShop.Models;
+
+namespace MyShop.Components;
+
+public class CategoryMenu : ViewComponent
+{
+    private readonly ICategoryRepository _categoryRepository;
+
+    public CategoryMenu(ICategoryRepository categoryRepository)
+    {
+        _categoryRepository = categoryRepository;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        //var categories = _categoryRepository.AllCategories.OrderBy(c => c.Name).ToList();
+        return View(_categoryRepository.AllCategories.OrderBy(c => c.Name).ToList());
+    }
+}
