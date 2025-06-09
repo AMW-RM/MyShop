2a5cb48a192a610bcaeea5e4ec9bbc94ad91ae12
Rafał Mąka <r.maka@amw-it.pl>
2025-06-09 17:07:14 +0200

    Dodanie TagHelper

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyShop.Models;
+
+namespace MyShop.Controllers;
+
+public class ContactController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
--- Models/Category.cs
+++ Models/Category.cs
@@ -1,9 +1,17 @@
 ﻿namespace MyShop.Models;
 
-public class Category
+public class Category : IComparable<Category>
 {
     public int CategoryId { get; set; }
     public string Name { get; set; } = string.Empty;
     public string? Description { get; set; }
-    public List<Product> Products { get; set; } 
+    public List<Product> Products { get; set; }
+
+    public int CompareTo(Category? other)
+    {
+        if (other == null) return 1;
+
+        return string.Compare(this.Name, other.Name, StringComparison.Ordinal);
+        
+    }
 }
