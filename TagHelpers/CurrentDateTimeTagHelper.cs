41f2b00ebc36ee461ba37c8eedcda23d637a7cd9
Rafał Mąka <r.maka@amw-it.pl>
2025-06-09 18:01:21 +0200

    Dodanie TagHelper CurrentDateTime

---
+++ TagHelpers/CurrentDateTimeTagHelper.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Identity.Client;
+using Microsoft.VisualBasic;
+
+namespace MyShop.TagHelpers;
+
+public class CurrentDateTimeTagHelper : TagHelper
+{
+    public string DateFormat { get; set; }
+    public string OutputTag { get; set; } = "span";
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        output.TagName = OutputTag;
+        output.TagMode = TagMode.StartTagAndEndTag;
+
+        var currentTime = DateTime.Now;
+        var formattedTime = string.Empty;
+        if (string.IsNullOrEmpty(DateFormat))
+        {
+            formattedTime = currentTime.ToString("dd-MM-yyyy HH:mm:ss");
+        }
+        else
+        {
+            try
+            {
+                formattedTime = currentTime.ToString(DateFormat);
+            }
+            catch (Exception)
+            {
+                formattedTime = $"{currentTime.ToString("dd-MM-yyyy HH:mm:ss")} (Błędny format: {DateFormat})";
+            }
+        }
+        output.Content.SetHtmlContent(formattedTime);
+    }
+}
